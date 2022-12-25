b78ef3395b8136bf0804ede13c21f7e1319fceda
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2022-12-25 19:20:00 +0530

    Add files via upload

---
+++ 291.) to print sum of first digit of three num using function.py
@@ -0,0 +1,10 @@
+def PRACHI(a,b,c):
+    a = str(a)[0]
+    b = str(b)[0]
+    c = str(c)[0]
+    print(int(a) + int(b) + int(c))
+
+a = int(input("ENTER A NUMBER : "))
+b = int(input("ENTER A NUMBER : "))
+c = int(input("ENTER A NUMBER : "))
+PRACHI(a,b,c)
