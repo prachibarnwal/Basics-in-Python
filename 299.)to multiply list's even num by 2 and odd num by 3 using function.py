132ba06bb501b6ceb05e8b1b58d0b0d0ba55cdb5
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2022-12-27 13:50:25 +0530

    Add files via upload

---
+++ 299.)to multiply list's even num by 2 and odd num by 3 using function.py
@@ -0,0 +1,12 @@
+#7
+def ODDEVEN(L):
+    for ch in L:
+        if ch % 2 == 0:
+            L[ch] *= 2
+        else:
+            L[ch] *= 3
+    return L
+
+L = list(eval(input("Enter a List Of Numbers : ")))
+a = ODDEVEN(L)
+print(a)
